a90da2f6b98acb181c1425ab2c29a2a597c7c29a
Tim-VandenBorre <69650471+Tim-VandenBorre@users.noreply.github.com>
2020-11-04 19:45:02 +0100

    Main activity layout, createmedication activity + layout. Use startActivityForResult for passing data between activities + logging data. English & Dutch for app.

---
+++ Medication_Reminder/app/src/main/java/com/example/medication_reminder/CreateMedicationActivity.java
@@ -0,0 +1,68 @@
+package com.example.medication_reminder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateMedicationActivity extends AppCompatActivity {
+
+    private Button saveMedication;
+    private EditText input_name, input_description, input_quantity, input_start_date, input_end_date, input_extra_information;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createmedication);
+
+        // Id's ophalen van de input fields.
+        saveMedication = findViewById(R.id.save_medication_button);
+        input_name = findViewById(R.id.input_name);
+        input_description = findViewById(R.id.input_description);
+        input_quantity = findViewById(R.id.input_quantity);
+        input_start_date = findViewById(R.id.input_start_date);
+        input_end_date = findViewById(R.id.input_end_date);
+        input_extra_information = findViewById(R.id.input_extra_information);
+
+        saveMedication.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveMedication();
+            }
+        });
+    }
+    
+    public void saveMedication(){
+        String name, description, quantity, start_date, end_date, extra_information;
+
+        // text value ophalen van alle fields.
+        name = input_name.getText().toString();
+        description = input_description.getText().toString();
+        quantity = input_quantity.getText().toString();
+        start_date = input_start_date.getText().toString();
+        end_date = input_end_date.getText().toString();
+        extra_information = input_extra_information.getText().toString();
+
+        /*
+            Geef waardes terug met de intent. Geraadpleegd op 4/11/2020.
+            https://www.javatpoint.com/android-startactivityforresult-example
+         */
+        // aanmaken intent & waardes toevoegen.
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("name", name);
+        intent.putExtra("description", description);
+        intent.putExtra("quantity", quantity);
+        intent.putExtra("start_date", start_date);
+        intent.putExtra("end_date", end_date);
+        intent.putExtra("extra_information", extra_information);
+
+        // toevoegen result code & intent.
+        setResult(RESULT_OK, intent);
+
+        // Response terug sturen naar onActivityResult.
+        finish();
+    }
+}
--- Medication_Reminder/app/src/main/java/com/example/medication_reminder/MainActivity.java
+++ Medication_Reminder/app/src/main/java/com/example/medication_reminder/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.medication_reminder;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button addMedication;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addMedication = findViewById(R.id.add_Medication_button);
+
+        addMedication.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addMedication();
+            }
+        });
+    }
+    /*
+        startActivityForResult. Geraadpleegd op 4/11/2020
+        https://developer.android.com/training/basics/intents/result
+        https://developer.android.com/reference/android/app/Activity#startActivityForResult(android.content.Intent,%20int)
+        https://stuff.mit.edu/afs/sipb/project/android/docs/training/basics/intents/result.html
+        Aanmaken van intent voor navigatie naar CreateMedicationActivity. Krijgt een intent & requestcode mee(200 voor OK).
+     */
+    public void addMedication(){
+        Intent intent = new Intent(this, CreateMedicationActivity.class);
+        startActivityForResult(intent, 200);
+    }
+    /*
+        onActivityResult. Geraadpleegd op 4/11/2020.
+        https://developer.android.com/reference/android/app/Activity#onActivityResult(int,%20int,%20android.content.Intent)
+        override deze methode. Het is de response op de startActivityForResult.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 200 && resultCode == RESULT_OK && data != null){
+
+            String name = data.getStringExtra("name");
+            String description = data.getStringExtra("description");
+            String quantity = data.getStringExtra("quantity");
+            String start_date = data.getStringExtra("start_date");
+            String end_date = data.getStringExtra("end_date");
+            String extra_information = data.getStringExtra("extra_information");
+
+            Log.e("name", name);
+            Log.e("description", description);
+            Log.e("quantity", quantity);
+            Log.e("start_date", start_date);
+            Log.e("end_date", end_date);
+            Log.e("extra_information", extra_information);
+        }
     }
 }
