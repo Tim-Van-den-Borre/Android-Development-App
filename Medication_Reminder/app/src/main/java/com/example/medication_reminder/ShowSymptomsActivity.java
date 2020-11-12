12882735b8e9711c1cd7bd8f71df670e9b3d31b0
Tim-Van-den-Borre <72195069+Tim-Van-den-Borre@users.noreply.github.com>
2020-11-12 16:38:52 +0100

    Api url

--- Medication_Reminder/app/src/main/java/com/example/medication_reminder/ShowSymptomsActivity.java
+++ Medication_Reminder/app/src/main/java/com/example/medication_reminder/ShowSymptomsActivity.java
@@ -38,6 +38,7 @@ public class ShowSymptomsActivity extends AppCompatActivity{
         setContentView(R.layout.activity_showsymptoms);
 
         // url maken voor de api call.
+        // api url: https://www.ehealthme.com/api/v1/docs/
         URL = "https://www.ehealthme.com/api/v1/ds/" + medication_name.toLowerCase() + "/weakness";
 
         makeApiCallAsync(URL);
